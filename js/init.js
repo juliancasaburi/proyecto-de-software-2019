3b4061ebe66aa7bb770d81d76e7beb942b43b1dd
Casaburi, Julián <juliancasaburi@gmail.com>
2019-10-07 21:25:13 -0300

    Agregar administrador.html y modificar sidenav

--- js/init.js
+++ js/init.js
@@ -6,6 +6,7 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.fixed-action-btn').floatingActionButton();
+    $(".dropdown-trigger").dropdown();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
