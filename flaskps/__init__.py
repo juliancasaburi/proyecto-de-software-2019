d41840c0332d261127e1f6a7e3ddf60abcbb28b8
Casaburi, Julián <juliancasaburi@gmail.com>
2019-10-14 19:30:08 -0300

    Exportar función is_authenticated a Jinja2

--- flaskps/__init__.py
+++ flaskps/__init__.py
@@ -1,6 +1,7 @@
 from os import path
 from flask import Flask, render_template
 from flaskps.config import Config
+from flaskps.helpers import auth as helper_auth
 
 # Resources
 from flaskps.resources import auth
@@ -8,6 +9,9 @@ from flaskps.resources import auth
 # Configuración inicial de la app
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Funciones que se exportan al contexto de Jinja2
+app.jinja_env.globals.update(is_authenticated=helper_auth.authenticated)
 
 # Autenticación
 app.add_url_rule("/login", 'auth_login', auth.login)
