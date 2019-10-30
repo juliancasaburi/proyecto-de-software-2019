ece4e302c1b4864ec88869a409aa2243825425d2
Tadeo Velis <tadeovelis@gmail.com>
2019-10-30 12:51:59 -0300

    Agregar validación al form del login con errores descriptivos

---
+++ flaskps/forms/form_login.py
@@ -0,0 +1,9 @@
+from flask import flash
+from flask_wtf import FlaskForm
+from wtforms import ValidationError, StringField, PasswordField
+from wtforms.validators import InputRequired, DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username', [InputRequired(message='Complete el nombre de usuario')])
+    password = PasswordField('password', validators=[DataRequired(message='Complete la contraseña')])
--- flaskps/resources/auth.py
+++ flaskps/resources/auth.py
@@ -1,6 +1,12 @@
+import pickle
+
 from flask import Flask, redirect, render_template, request, url_for, session, flash
+from wtforms import ValidationError
+
 from flaskps.db import get_db
 from flask_bcrypt import Bcrypt
+
+from flaskps.forms.form_login import LoginForm
 from flaskps.helpers.auth import authenticated
 from flaskps.models.user import User
 from flaskps.helpers import siteconfig as helper_siteconfig
@@ -18,27 +24,40 @@ def login():
 
 
 def authenticate():
-    params = request.form
 
-    User.db = get_db()
-    user = User.find_by_user(params['username'])
-    if user and user['activo'] == 1 and bcrypt.check_password_hash(user['password'], params['password']):
+    form = LoginForm()
 
-        SiteConfig.db = get_db()
-        config = helper_siteconfig.get_config()
-        modo_mantenimiento = config['modo_mantenimiento']
+    if form.validate_on_submit():
+        params = request.form
+        User.db = get_db()
+        user = User.find_by_user(params['username'])
 
-        if modo_mantenimiento == '1' and (not User.has_role(params['username'], 'administrador')):
-            flash("Sitio en mantenimiento", "error")
+        if user and user['activo'] == 1 and bcrypt.check_password_hash(user['password'], params['password']):
+            SiteConfig.db = get_db()
+            config = helper_siteconfig.get_config()
+            modo_mantenimiento = config['modo_mantenimiento']
+
+            if modo_mantenimiento == '1' and (not User.has_role(params['username'], 'administrador')):
+                flash("Sitio en mantenimiento", "error")
+            else:
+                session['user'] = user['username']
+                flash("La sesión se inició correctamente", "success")
+                return redirect(url_for('user_dashboard'))
+
+        elif user and user['activo'] == 0:
+            flash("Su cuenta está bloqueada", "error")
         else:
-            session['user'] = user['username']
-            flash("La sesión se inició correctamente", "success")
-            return redirect(url_for('user_dashboard'))
+            flash("Usuario o clave incorrecto", "error")
 
-    elif user and user['activo'] == 0:
-        flash("Su cuenta está bloqueada", "error")
+    # TODO: Mensajes de error
     else:
-        flash("Usuario o clave incorrecto", "error")
+        if len(form.errors) == 2:
+            flash("Complete los campos para poder loguearse", "error")
+        else:
+            error_msg = ''.join(form.errors[list(form.errors)[0]]).strip("'[]")
+            flash(error_msg, "error")
+
+
     return redirect(url_for('auth_login'))
 
 
