ede1d3a4c216bd010751e217db1393ad3b6d3689
Tadeo Velis <tadeovelis@gmail.com>
2019-12-02 13:56:01 -0300

    Agregar el modal de asociar ciclos a taller

--- flaskps/resources/administracion.py
+++ flaskps/resources/administracion.py
@@ -1,8 +1,19 @@
 from flask import render_template
 
+from flaskps.db import get_db
+from flaskps.models.ciclo_lectivo import CicloLectivo
+
 
 def administracion():
+    CicloLectivo.db = get_db()
+    ciclos = CicloLectivo.all()
+
+    for ciclo in ciclos:
+        ciclo["fecha_ini"] = ciclo["fecha_ini"].strftime("%d-%m-%Y")
+        ciclo["fecha_fin"] = ciclo["fecha_fin"].strftime("%d-%m-%Y")
+
     return render_template(
         "user/moduloadministrativo.html",
+        ciclos=ciclos
         #username=user["username"],
     )
